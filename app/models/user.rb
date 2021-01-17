d3ebc48e45aa2f4449cc8be837661fdd12ff9b91
maksimpanarin <m-panarin@m-panarin-4.local>
2021-01-17 19:57:17 +0200

    Add rspec, User model and test example

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+# Schema information
+
+# Table name: users
+
+# id         :integer      not null, primary key
+# first_name :string
+# last_name  :string
+# email      :string
+# active     :boolean      default: true
+# created_at :datetime     not null
+# updated_at :datetime     not null
+
+
+class User < ApplicationRecord
+end
