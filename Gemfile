bcb23b618c22a59d2e0cd031004a3f583b7634cf
maksimpanarin <maksimpanarin88@gmail.com>
2021-03-09 22:43:41 +0200

    add kaminari, add pagination to users.index

--- Gemfile
+++ Gemfile
@@ -56,4 +56,5 @@ gem 'active_model_serializers', '~> 0.10.0'
 gem 'devise'
 gem 'devise-jwt'
 gem 'dotenv-rails'
+gem 'kaminari'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all
+    @users = User.order(:id).page(params[:page])
     render json: @users
   end
 
